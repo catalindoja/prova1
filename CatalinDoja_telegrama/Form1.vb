6d9e3eb197417523411b7c568ee0228c787f125e
catalindoja <cdojamihai@gmail.com>
2018-03-06 16:15:42 +0100

    missatge de confirmació

--- CatalinDoja_telegrama/Form1.vb
+++ CatalinDoja_telegrama/Form1.vb
@@ -6,6 +6,7 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'tabs, comes, punt i coma i finalment espai. com separador
         'streamreader peek split
+		'
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
